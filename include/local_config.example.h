16370da3c05be7af4ba77018e601e3b5692e7c0a
omergershonwork-commits <omergershonwork@gmail.com>
2026-08-01 23:45:21 +0300

    TV-009: add local Wi-Fi configuration template

---
+++ include/local_config.example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Copy this file to include/local_config.h and replace the values locally.
+// local_config.h is ignored by Git so credentials are never committed.
+
+#define GC_WIFI_SSID "YOUR_WIFI_NAME"
+#define GC_WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+#define GC_XIAOMI_TV_IP "192.168.1.100"
