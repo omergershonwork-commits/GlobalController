99c308bb87cf135db38d84ab0447e0a718c8ca21
omergershonwork-commits <omergershonwork@gmail.com>
2026-08-01 19:05:56 +0300

    TV-006: return domain command bindings from keyboard mapper

--- src/input/KeyboardCommandMapper.h
+++ src/input/KeyboardCommandMapper.h
@@ -2,16 +2,9 @@
 
 #include <M5Cardputer.h>
 
-#include "domain/TvCommand.h"
-
-struct KeyboardCommand {
-    bool matched;
-    TvCommand command;
-    const char* label;
-    bool repeatable;
-};
+#include "domain/CommandBinding.h"
 
 class KeyboardCommandMapper final {
 public:
-    static KeyboardCommand map(const Keyboard_Class::KeysState& state);
+    static CommandBinding map(const Keyboard_Class::KeysState& state);
 };
