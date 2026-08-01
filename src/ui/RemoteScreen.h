289e26c64e4aa6d01e661ca4030cc43a32165a3c
omergershonwork-commits <omergershonwork@gmail.com>
2026-08-01 19:31:50 +0300

    TV-007: add remote screen interface

---
+++ src/ui/RemoteScreen.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "app/RemoteApplication.h"
+#include "profile/TvProfile.h"
+
+class RemoteScreen final {
+public:
+    explicit RemoteScreen(const TvProfile& profile);
+
+    void begin();
+    void showReady();
+    void showEvent(const RemoteEvent& event);
+
+private:
+    void drawLayout();
+    void drawStatus(const char* state, const char* detail, std::uint16_t accentColor);
+
+    const TvProfile& profile_;
+};
