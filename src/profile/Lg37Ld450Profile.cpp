1a2be9bc10aeceb82d889b2f94dc74be084492c6
omergershonwork-commits <omergershonwork@gmail.com>
2026-08-01 19:01:09 +0300

    TV-005: mark tested LG commands verified

--- src/profile/Lg37Ld450Profile.cpp
+++ src/profile/Lg37Ld450Profile.cpp
@@ -6,19 +6,19 @@ constexpr std::int8_t kNoRepeats = 0;
 
 constexpr TvProfileEntry kEntries[] = {
     {TvCommand::Power, {IrProtocol::Nec, kLgAddress, 0x08, kNoRepeats}, CodeVerification::VerifiedOnDevice},
-    {TvCommand::VolumeUp, {IrProtocol::Nec, kLgAddress, 0x02, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::VolumeDown, {IrProtocol::Nec, kLgAddress, 0x03, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::Mute, {IrProtocol::Nec, kLgAddress, 0x09, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::ChannelUp, {IrProtocol::Nec, kLgAddress, 0x00, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::ChannelDown, {IrProtocol::Nec, kLgAddress, 0x01, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::NavigateUp, {IrProtocol::Nec, kLgAddress, 0x40, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::NavigateDown, {IrProtocol::Nec, kLgAddress, 0x41, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::NavigateLeft, {IrProtocol::Nec, kLgAddress, 0x07, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::NavigateRight, {IrProtocol::Nec, kLgAddress, 0x06, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::Ok, {IrProtocol::Nec, kLgAddress, 0x44, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::Back, {IrProtocol::Nec, kLgAddress, 0x28, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::Home, {IrProtocol::Nec, kLgAddress, 0x43, kNoRepeats}, CodeVerification::Provisional},
-    {TvCommand::Input, {IrProtocol::Nec, kLgAddress, 0x0B, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::VolumeUp, {IrProtocol::Nec, kLgAddress, 0x02, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::VolumeDown, {IrProtocol::Nec, kLgAddress, 0x03, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::Mute, {IrProtocol::Nec, kLgAddress, 0x09, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::ChannelUp, {IrProtocol::Nec, kLgAddress, 0x00, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::ChannelDown, {IrProtocol::Nec, kLgAddress, 0x01, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::NavigateUp, {IrProtocol::Nec, kLgAddress, 0x40, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::NavigateDown, {IrProtocol::Nec, kLgAddress, 0x41, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::NavigateLeft, {IrProtocol::Nec, kLgAddress, 0x07, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::NavigateRight, {IrProtocol::Nec, kLgAddress, 0x06, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::Ok, {IrProtocol::Nec, kLgAddress, 0x44, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::Back, {IrProtocol::Nec, kLgAddress, 0x28, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::Home, {IrProtocol::Nec, kLgAddress, 0x43, kNoRepeats}, CodeVerification::VerifiedOnDevice},
+    {TvCommand::Input, {IrProtocol::Nec, kLgAddress, 0x0B, kNoRepeats}, CodeVerification::VerifiedOnDevice},
 };
 }  // namespace
 
