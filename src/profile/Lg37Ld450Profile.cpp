165857d004bc10dd90c447880b125d8dfeef79a0
omergershonwork-commits <omergershonwork@gmail.com>
2026-08-01 20:02:28 +0300

    TV-008: add provisional Xiaomi MiTV-MSSP3 IR profile

--- src/profile/Lg37Ld450Profile.cpp
+++ src/profile/Lg37Ld450Profile.cpp
@@ -2,9 +2,10 @@
 
 namespace {
 constexpr std::uint16_t kLgAddress = 0x04;
+constexpr std::uint16_t kXiaomiDevice = 0x86;
 constexpr std::int8_t kNoRepeats = 0;
 
-constexpr TvProfileEntry kEntries[] = {
+constexpr TvProfileEntry kLgEntries[] = {
     {TvCommand::Power, {IrProtocol::Nec, kLgAddress, 0x08, kNoRepeats}, CodeVerification::VerifiedOnDevice},
     {TvCommand::VolumeUp, {IrProtocol::Nec, kLgAddress, 0x02, kNoRepeats}, CodeVerification::VerifiedOnDevice},
     {TvCommand::VolumeDown, {IrProtocol::Nec, kLgAddress, 0x03, kNoRepeats}, CodeVerification::VerifiedOnDevice},
@@ -20,6 +21,35 @@ constexpr TvProfileEntry kEntries[] = {
     {TvCommand::Home, {IrProtocol::Nec, kLgAddress, 0x43, kNoRepeats}, CodeVerification::VerifiedOnDevice},
     {TvCommand::Input, {IrProtocol::Nec, kLgAddress, 0x0B, kNoRepeats}, CodeVerification::VerifiedOnDevice},
 };
+
+constexpr TvProfileEntry kXiaomiEntries[] = {
+    {TvCommand::Power, {IrProtocol::XiaomiRcmm, 0x3C, 0xCC, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::VolumeUp, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0x0E, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::VolumeDown, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0x0F, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::Mute, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0xA1, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::NavigateUp, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0x05, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::NavigateDown, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0x06, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::NavigateLeft, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0x0B, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::NavigateRight, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0x0C, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::Ok, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0x0D, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::Back, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0x07, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::Home, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0x08, kNoRepeats}, CodeVerification::Provisional},
+    {TvCommand::Input, {IrProtocol::XiaomiRcmm, kXiaomiDevice, 0x01, kNoRepeats}, CodeVerification::Provisional},
+};
+
+template <std::size_t EntryCount>
+const TvProfileEntry* findEntry(
+    const TvProfileEntry (&entries)[EntryCount],
+    TvCommand command
+) {
+    for (const auto& entry : entries) {
+        if (entry.command == command) {
+            return &entry;
+        }
+    }
+
+    return nullptr;
+}
 }  // namespace
 
 const char* Lg37Ld450Profile::brand() const {
@@ -31,11 +61,17 @@ const char* Lg37Ld450Profile::model() const {
 }
 
 const TvProfileEntry* Lg37Ld450Profile::find(TvCommand command) const {
-    for (const auto& entry : kEntries) {
-        if (entry.command == command) {
-            return &entry;
-        }
-    }
+    return findEntry(kLgEntries, command);
+}
 
-    return nullptr;
+const char* XiaomiMiTvMssp3Profile::brand() const {
+    return "Xiaomi";
+}
+
+const char* XiaomiMiTvMssp3Profile::model() const {
+    return "MiTV-MSSP3";
+}
+
+const TvProfileEntry* XiaomiMiTvMssp3Profile::find(TvCommand command) const {
+    return findEntry(kXiaomiEntries, command);
 }
