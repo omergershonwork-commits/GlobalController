63bf1ad9c65502d8dcc30c252466d7e5903ee6f7
omergershonwork-commits <omergershonwork@gmail.com>
2026-08-02 12:56:59 +0300

    TV-009: make television address discoverable at runtime

--- src/config/DeviceConfig.h
+++ src/config/DeviceConfig.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <Arduino.h>
-#include <WiFi.h>
-
 #if __has_include(<local_config.h>)
 #include <local_config.h>
 #endif
@@ -15,33 +12,16 @@
 #define GC_WIFI_PASSWORD ""
 #endif
 
-#ifndef GC_XIAOMI_TV_IP
-#define GC_XIAOMI_TV_IP ""
-#endif
-
 struct DeviceConfig {
     const char* wifiSsid;
     const char* wifiPassword;
-    const char* xiaomiTvIp;
 
     bool hasWifiCredentials() const {
-        return wifiSsid != nullptr &&
-               wifiSsid[0] != '\0' &&
-               wifiPassword != nullptr &&
-               wifiPassword[0] != '\0';
-    }
-
-    bool parseXiaomiTvIp(IPAddress& address) const {
-        if (xiaomiTvIp == nullptr || xiaomiTvIp[0] == '\0') {
-            return false;
-        }
-
-        return address.fromString(xiaomiTvIp);
+        return wifiSsid != nullptr && wifiSsid[0] != '\0';
     }
 };
 
 constexpr DeviceConfig kDeviceConfig{
     GC_WIFI_SSID,
     GC_WIFI_PASSWORD,
-    GC_XIAOMI_TV_IP,
 };
